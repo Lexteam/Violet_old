b2b5eb7d81468a27ae9491bec137838a6ef8b2c3
Jamie Mansfield <dev@jamierocks.uk>
2015-12-30 00:46:00 +0000

    Build a minified css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp');
 
 var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 gulp.task('scss', function () {
     gulp.src('./scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['scss']);
+gulp.task('build', ['scss'], function () {
+    gulp.src('dist/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['build']);
